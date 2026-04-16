b29ed113405dedf996fbd538f2b8fef2dcd07102
Marcos Vogado <marcoshcvogado@gmail.com>
2026-04-16 17:09:40 -0300

    feat: Parcial login endpoint

---
+++ ScreenSound.Web/Services/AuthAPI.cs
@@ -0,0 +1,13 @@
+﻿using System.Security;
+
+namespace ScreenSound.Web.Services;
+
+public class AuthAPI(IHttpClientFactory factory)
+{
+    private readonly HttpClient _HttpClient = factory.CreateClient("API");
+    
+    public Task<AuthResponse> LoginAsync(string email, SecureString senha)
+    {
+
+    }
+}
